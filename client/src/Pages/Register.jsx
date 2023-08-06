8f5853dcde36cf06dcaede327b7040186d382257
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-08-06 16:42:40 +0530

    register page

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
 
 function Register() {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to='/login'>Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type='text' name='name' defaultValue='vivek' />
+        <FormRow type='text' name='location' defaultValue='earth' />
+        <FormRow type='email' name='email' defaultValue='vivek.koyani@gmail.com' />
+        <FormRow type='password' name='password' defaultValue='vivek123' />
+        <FormRow type='text' name='name' defaultValue='vivek' />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>Login</Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
