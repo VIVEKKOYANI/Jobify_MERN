69791d275741d52629bcaff88b6b6e58a465f4d9
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-08-06 16:51:01 +0530

    login page

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
 
 function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to='/register'>Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type='email' name='email' defaultValue='vivek.koyani@gmail.com' />
+        <FormRow type='password' name='password' defaultValue='vivek123' />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <button type='button' className='btn btn-block'>
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>Register</Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
