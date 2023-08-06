6d6230ac190d1bbc2ebeffd997d42961393abad3
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-08-06 12:36:56 +0530

    error page add

--- client/src/Pages/Error.jsx
+++ client/src/Pages/Error.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import { useRouteError } from 'react-router-dom'
-import { Line } from 'recharts'
+import { Link, useRouteError } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
 
 function Error() {
   const error = useRouteError();
-  console.log(erro);
+  
+  if(error.status === 404){
+    return <Wrapper>
+      <div>
+        <img src={img} alt='not found'></img>
+        <h3>Ohh! page not found</h3>
+        <p>we can't seem to find the page you are looking for</p>
+        <Link to="/dashboard">back home</Link>
+      </div>
+    </Wrapper>
+  }
   return (
     <div>
-      <h1>Error Page</h1>
-      <Line to="/">back home</Line>
+      <h3>something went wrong</h3>
     </div>
   )
 }
