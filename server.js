4f5c0fa5d76dd172c7d5d25732ff1d2422d9dd15
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-08-19 13:54:14 +0530

    changes for get jobs api route add in localy

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ dotenv.config();
 import express from 'express';
  const app = express();
  import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+let jobs = [
+   {id: nanoid(), company: 'apple', position: 'front-end'},
+   {id: nanoid(), company: 'google', position: 'front-end'}
+]
 
  if(process.env.NODE_ENV === 'development'){
      app.use(morgan('dev'));
@@ -17,6 +23,10 @@ import express from 'express';
  app.post('/', (req, res) => {
     console.log(req);
     res.json({message: 'data received', data: req.body });
+ });
+
+ app.get('/api/v1/jobs', (req, res) => {
+   res.status(200).json({jobs})
  })
 
  const port = process.env.PORT || 5100
