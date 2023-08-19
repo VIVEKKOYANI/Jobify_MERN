5a7857c9938cc0cdce955706c9c341fa7a448016
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-08-19 10:45:39 +0530

    add json middleware

--- server.js
+++ server.js
@@ -2,9 +2,16 @@
 
  const app = express();
 
+ app.use(express.json());
+
  app.get('/', (req, res) => {
     res.send('Hello World');
  });
+
+ app.post('/', (req, res) => {
+    console.log(req);
+    res.json({message: 'data received', data: req.body });
+ })
 
  app.listen(5100, () => {
     console.log("server running...");
