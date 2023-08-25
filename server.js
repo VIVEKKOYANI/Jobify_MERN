a9018e61b0dbc68a6520b8e6ca3b91732ddad36e
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-08-25 19:52:21 +0530

    add router and controller

--- server.js
+++ server.js
@@ -1,95 +1,39 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
- const app = express();
- import morgan from 'morgan';
-import { nanoid } from 'nanoid';
+const app = express();
+import morgan from 'morgan';
 
-let jobs = [
-   {id: nanoid(), company: 'apple', position: 'front-end'},
-   {id: nanoid(), company: 'google', position: 'front-end'}
-]
+// routers
+import jobRouter from './routes/jobRouter.js';
 
- if(process.env.NODE_ENV === 'development'){
-     app.use(morgan('dev'));
- }
+if (process.env.NODE_ENV === 'development') {
+   app.use(morgan('dev'));
+}
 
- app.use(express.json());
+app.use(express.json());
 
- app.get('/', (req, res) => {
-    res.send('Hello World');
- });
+app.get('/', (req, res) => {
+   res.send('Hello World');
+});
 
- app.post('/', (req, res) => {
-    console.log(req);
-    res.json({message: 'data received', data: req.body });
- });
+app.post('/', (req, res) => {
+   console.log(req);
+   res.json({ message: 'data received', data: req.body });
+});
 
- // GET ALL JOBS
- app.get('/api/v1/jobs', (req, res) => {
-   res.status(200).json({jobs})
- })
+app.use('/api/v1/jobs', jobRouter);
 
-//CREATE JOB
- app.post('/api/v1/jobs', (req, res) => {
-   const {company, position} = req.body;
-   if(!company || !position){
-      return res.status(400).json({msg: 'please provide company and position'});
-   }
-   const id = nanoid(10);
-   const job = {id, company, position};
-   jobs.push(job);
-   res.status(200).json({job})
- });
+app.use('*', (req, res) => {
+   res.status(404).json({ msg: 'not found' });
+})
 
- //GET SINGLE JOB
- app.get('/api/v1/jobs/:id', (req, res) => {
-   const {id} = req.params;
-   const job = jobs.find((job) => job.id === id);
-   if(!job){
-      return res.status(400).json({msg: `no job with id ${id}`});
-   }
-   res.status(200).json({job});
- });
+app.use((err, req, res, next) => {
+   res.status(500).json({ msg: 'something went wrong' });
+})
 
- //EDIT JOB
- app.patch('/api/v1/jobs/:id', (req, res) => {
-   const {company, position} = req.body;
-   if(!company || !position){
-      return res.status(400).json({msg: 'please provide company and position'});
-   }
-   const {id} = req.params;
-   const job = jobs.find((job) => job.id === id);
-   if(!job){
-      return res.status(400).json({msg: `no job with id ${id}`});
-   }
-   job.company = company;
-   job.position = position;
-   res.status(200).json({msg: 'job modified' ,job});
- });
+const port = process.env.PORT || 5100
 
- //DELETE JOB
- app.delete('/api/v1/jobs/:id', (req, res) => {
-   const {id} = req.params;
-   const job = jobs.find((job) => job.id === id);
-   if(!job){
-      return res.status(400).json({msg: `no job with id ${id}`});
-   }
-   const newJobs = jobs.filter((job) => job.id !== id);
-   jobs = newJobs;
-   res.status(200).json({msg: 'job deleted' });
- });
-
- app.use('*', (req, res) => {
-   res.status(404).json({msg: 'not found'});
- })
-
- app.use((err, req, res, next) => {
-   res.status(500).json({msg: 'something went wrong'});
- })
-
- const port = process.env.PORT || 5100
-
- app.listen(port, () => {
-    console.log("server running...");
- })
+app.listen(port, () => {
+   console.log("server running...");
+})
