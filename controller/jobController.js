5bd2ea7cc1581b14921a9a279c2d8b1a708a7983
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-08-27 10:09:55 +0530

    changes for validation and controller

--- controller/jobController.js
+++ controller/jobController.js
@@ -1,6 +1,5 @@
 import Job from '../models/jobModel.js';
 import { StatusCodes } from 'http-status-codes';
-import { NotFoundError } from '../errors/customError.js';
 
 export const getAllJobs = async (req, res) => {
    const jobs = await Job.find({});
@@ -13,24 +12,16 @@ export const createJob = async (req, res) => {
 };
 
 export const getJob = async (req, res) => {
-   const { id } = req.params;
-   const job = await Job.findById(id);
-   if (!job) throw new NotFoundError(`no job with id ${id}`);
+   const job = await Job.findById(req.params.id);
    res.status(StatusCodes.OK).json({ job });
 };
 
 export const updateJob = async (req, res) => {
-   
-   const { id } = req.params;
-   const updatedJob = await Job.findByIdAndUpdate(id , res.body, {new: true})
-   if (!updatedJob) throw new NotFoundError(`no job with id ${id}`);
+   const updatedJob = await Job.findByIdAndUpdate(req.params.id , res.body, {new: true})
    res.status(StatusCodes.OK).json({ msg: 'job modified', job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
-   const { id } = req.params;
-   const removeJob = await Job.findByIdAndDelete(id);
-   if (!removeJob) throw new NotFoundError(`no job with id ${id}`);
-   
+   const removeJob = await Job.findByIdAndDelete(req.params.id);   
    res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removeJob });
 };
--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,7 +1,8 @@
 import { body, param, validationResult } from "express-validator";
-import { BadRequestError } from "../errors/customError.js";
+import { BadRequestError, NotFoundError } from "../errors/customError.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
 import mongoose from "mongoose";
+import Job from "../models/jobModel.js";
 
 const withValidationErrors = (validationValues) => {
   return [
@@ -10,6 +11,9 @@ const withValidationErrors = (validationValues) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         const errorMassages = errors.array().map((error) => error.msg);
+        if (errorMassages[0].startsWith("no job")) {
+          throw new NotFoundError(errorMassages);
+        }
         throw new BadRequestError(errorMassages);
       }
       next();
@@ -30,7 +34,10 @@ export const validateJobInput = withValidationErrors([
 ]);
 
 export const validateIdParam = withValidationErrors([
-  param("id")
-    .custom((value) => mongoose.Types.ObjectId.isValid(value))
-    .withMessage("invalid MongoDB id"),
+  param("id").custom(async (value) => {
+    const isValidId = mongoose.Types.ObjectId.isValid(value);
+    if (!isValidId) throw new Error("invalid MongoDB id");
+    const job = await Job.findById(value);
+    if (!job) throw new NotFoundError(`no job with id ${value}`);
+  }),
 ]);
