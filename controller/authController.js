d57f676003af940d32b669903fdbe10e27764a97
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-08-28 09:36:12 +0530

    add auth login logic

--- controller/authController.js
+++ controller/authController.js
@@ -1,16 +1,21 @@
 import { StatusCodes } from "http-status-codes";
-import User from '../models/userModel.js';
-import { hashPassword } from "../utils/passwordUtils.js";
+import User from "../models/userModel.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customError.js";
 
 export const register = async (req, res) => {
-    const isFirstAccount  = (await User.countDocuments()) === 0;
-    req.body.role = isFirstAccount ? 'admin' : 'user';
-    const hashedPassword = await hashPassword(req.body.password);
-    req.body.password = hashedPassword;
-    const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json({ msg: 'user created' });
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? "admin" : "user";
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
-    res.send('login');;
+  const user = await User.findOne({ email: req.body.email });
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
+  if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
+  res.send("login");
 };
