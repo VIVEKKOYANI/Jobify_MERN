49ca4dff64852485a73f9fdab5ed5f471c4c65ec
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-08-29 18:31:46 +0530

    logout

--- controller/authController.js
+++ controller/authController.js
@@ -21,10 +21,18 @@ export const login = async (req, res) => {
 
   const token = createJWT({ userId: user._id, role: user.role });
   const oneDay = 1000 * 60 * 60 * 24;
-  res.cookie('token', token, {
+  res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === 'production'
+    secure: process.env.NODE_ENV === "production",
   });
-  res.status(StatusCodes.OK).json({ msg: 'user logged in' });
+  res.status(StatusCodes.OK).json({ msg: "user logged in" });
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
 };
