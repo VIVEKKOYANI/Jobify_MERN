3e7a23eb8cbdff2e1700527f753274706afc72ec
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-08-30 17:26:52 +0530

    array in role

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ import { authorizePermissions } from "../middleware/authMiddleware.js";
 const router = Router();
 
 router.get("/current-user", getCurrentUser);
-router.get("/admin/app-stats",authorizePermissions('admin'), getApplicationStats);
+router.get("/admin/app-stats",[authorizePermissions('admin'), getApplicationStats]);
 router.patch("/update-user",validateUpdateUserInput, updateUser);
 
 export default router;
