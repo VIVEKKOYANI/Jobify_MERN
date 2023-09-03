71226adc59984a6a2dcbc767b0e16e0595698392
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-09-03 10:36:34 +0530

    tostify setup

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -3,14 +3,17 @@ import {Form, redirect, useNavigation ,Link } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../components';
 import customFetch from '../utils/customFetch';
+import {toast} from 'react-toastify';
 
 export const action = async({request}) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
   try {
     await customFetch.post('/auth/register', data);
+    toast.success('Registration successful');
     return redirect('/login');
   } catch (error) {
+    toast.error(error?.response?.data?.msg);
     return error;
   }
 }
@@ -29,7 +32,7 @@ function Register() {
         <FormRow type='email' name='email' defaultValue='vivek.koyani@gmail.com' />
         <FormRow type='password' name='password' defaultValue='vivek123' />
         <button type='submit' className='btn btn-block' disabled={isSubmitting}>
-          {isSubmitting ? 'submitting...' : submit}
+          {isSubmitting ? 'submitting...' : 'submit'}
         </button>
         <p>
           Already a member?
