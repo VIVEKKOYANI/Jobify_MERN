995469aeba1f47af62283d8730b6d5d29afbf0c3
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-09-03 12:06:49 +0530

    userActionData hook

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
-import { Link, Form, redirect, useNavigation } from "react-router-dom";
+import { Link, Form, redirect, useNavigation, useActionData } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  const errors = {msg: ''}
+  if(data.password.length < 3){
+    errors.msg = 'password too short';
+    return errors
+  }
+
   try {
     await customFetch.post("/auth/login", data);
     toast.success("Login successful");
     return redirect("/dashboard");
   } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return error;
+    // toast.error(error?.response?.data?.msg);
+    errors.msg = error?.response?.data?.msg;
+    return errors;
   }
 };
 
