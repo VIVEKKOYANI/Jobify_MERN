ff2c5fea6583c7ea1d632d4b851da47fbffaae6b
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-09-03 12:57:45 +0530

    logout

--- client/src/Pages/DashboardLayout.jsx
+++ client/src/Pages/DashboardLayout.jsx
@@ -1,9 +1,10 @@
 import React, { createContext, useContext, useState } from "react";
-import { Outlet, redirect, useLoaderData } from "react-router-dom";
+import { Outlet, redirect, useLoaderData, useNavigate } from "react-router-dom";
 import Wrapper from "../assets/wrappers/Dashboard";
 import { BigSidebar, Navbar, SmallSidebar } from "../components";
 import { checkDefaultTheme } from "../App";
 import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 
 export const loader = async () => {
   try {
@@ -19,7 +20,7 @@ const DashboardContext = createContext();
 function DashboardLayout() {
   // temp
   const { user } = useLoaderData();
-
+  const navigate = useNavigate();
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme);
 
@@ -35,7 +36,9 @@ function DashboardLayout() {
   };
 
   const logoutUser = async () => {
-    console.log("logout user");
+    navigate('/');
+    await customFetch.get('/auth/logout');
+    toast.success('Logging out...')
   };
   return (
     <DashboardContext.Provider
