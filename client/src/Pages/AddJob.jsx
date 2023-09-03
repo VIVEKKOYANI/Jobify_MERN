b9f8fcca62af0d1c57d5442f53e1e476f8da6b69
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-09-03 17:28:40 +0530

    add job structure

--- client/src/Pages/AddJob.jsx
+++ client/src/Pages/AddJob.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { FormRow } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function AddJob() {
+  const { user } = useOutletContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   return (
-    <h1>AddJob</h1>
-  )
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">add job</h4>
+        <div className="form-center">
+          <FormRow type="text" name="position" />
+          <FormRow type="text" name="company" />
+          <FormRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            defaultValue={user.location}
+          />
+          <button
+            type="submit"
+            className="btn btn-block from-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "submitting" : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 }
 
-export default AddJob
+export default AddJob;
