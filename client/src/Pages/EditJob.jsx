43bd3a3eedc681747a500a3d3dec8509f446a37e
vivek koyani <vivek.koyani@gmail.com>
2023-09-17 11:30:21 +0530

    edit job jsx

--- client/src/Pages/EditJob.jsx
+++ client/src/Pages/EditJob.jsx
@@ -8,22 +8,60 @@ import customFetch from "../utils/customFetch";
 
 export const loader = async ({ params }) => {
   try {
-    const {data} = await customFetch.get(`/jobs/${params.id}`);
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
     return data;
   } catch (error) {
     toast.error(error.response.data.msg);
-    return redirect('/dashboard/all-jobs');
+    return redirect("/dashboard/all-jobs");
   }
 };
 
 export const action = async () => {
-  const {job} = useLoaderData();
+  const { job } = useLoaderData();
   console.log(job);
   return null;
 };
 
 function EditJob() {
-  return <h1>EditJob</h1>;
+  const { job } = useLoaderData();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">edit job</h4>
+        <div className="form-center">
+          <FormRow type="text" name="position" defaultValue={job.position} />
+          <FormRow type="text" name="company" defaultValue={job.company} />
+          <FormRow
+            type="text"
+            name="jobLocation"
+            labelText="job location"
+            defaultValue={job.jobLocation}
+          />
+          <FormRowSelect
+            name="jobStatus"
+            labelText="job status"
+            defaultValue={job.jobStatus}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            name="jobType"
+            labelText="job type"
+            defaultValue={job.jobType}
+            list={Object.values(JOB_TYPE)}
+          />
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "submiting..." : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 }
 
 export default EditJob;
