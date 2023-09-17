3c9f012127613d2c544c3466514ecb7410ba503d
vivek koyani <vivek.koyani@gmail.com>
2023-09-17 18:14:21 +0530

    admin page complete

--- client/src/Pages/Admin.jsx
+++ client/src/Pages/Admin.jsx
@@ -4,6 +4,7 @@ import customFetch from "../utils/customFetch";
 import Wrapper from "../assets/wrappers/StatsContainer";
 import { toast } from "react-toastify";
 import { redirect, useLoaderData } from "react-router-dom";
+import { StateItem } from "../components";
 
 export const loader = async () => {
   try {
@@ -19,7 +20,20 @@ function Admin() {
   const { users, jobs } = useLoaderData();
   return (
     <Wrapper>
-      <h1>Admin</h1>
+      <StateItem
+        title="current users"
+        count={users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StateItem
+        title="total jobs"
+        count={jobs}
+        color="#647acb"
+        bcg="#e0e8f9"
+        icon={<FaCalendarCheck />}
+      />
     </Wrapper>
   );
 }
