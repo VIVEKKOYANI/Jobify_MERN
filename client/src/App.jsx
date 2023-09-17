dcfcde3f481dd806bc6e6b40c4eb2e8e5e7b5f7e
vivek koyani <vivek.koyani@gmail.com>
2023-09-17 10:24:23 +0530

    edit job setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,77 +1,92 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { HomeLayout, Landing, Register, Login, DashboardLayout, Error, AddJob, Starts, AllJobs, Profile, Admin } from './Pages'
-import { action as registerAction } from './Pages/Register';
-import { action as loginAction } from './Pages/Login';
-import { loader as DashboardLoader } from './Pages/DashboardLayout';
-import { loader as allJobsLoader } from './Pages/AllJobs';
-import { action as  addJobAction} from './Pages/AddJob';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+  AddJob,
+  Starts,
+  AllJobs,
+  Profile,
+  Admin,
+  EditJob,
+} from "./Pages";
+import { action as registerAction } from "./Pages/Register";
+import { action as loginAction } from "./Pages/Login";
+import { loader as DashboardLoader } from "./Pages/DashboardLayout";
+import { loader as allJobsLoader } from "./Pages/AllJobs";
+import { action as addJobAction } from "./Pages/AddJob";
 
 export const checkDefaultTheme = () => {
-  const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
-  document.body.classList.toggle('dark-theme', isDarkTheme);
-  return isDarkTheme
-}
+  const isDarkTheme = localStorage.getItem("darkTheme") === "true";
+  document.body.classList.toggle("dark-theme", isDarkTheme);
+  return isDarkTheme;
+};
 
 checkDefaultTheme();
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomeLayout />,
     errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <Landing />
+        element: <Landing />,
       },
       {
-        path: '/login',
+        path: "/login",
         element: <Login />,
         action: loginAction,
       },
       {
-        path: '/register',
+        path: "/register",
         element: <Register />,
         action: registerAction,
       },
       {
-        path: '/dashboard',
+        path: "/dashboard",
         element: <DashboardLayout />,
         loader: DashboardLoader,
         children: [
           {
             index: true,
             element: <AddJob />,
-            action: addJobAction
+            action: addJobAction,
           },
           {
-            path: 'starts',
-            element: <Starts />
+            path: "starts",
+            element: <Starts />,
           },
           {
-            path: 'all-jobs',
+            path: "all-jobs",
             element: <AllJobs />,
-            loader: allJobsLoader
+            loader: allJobsLoader,
           },
           {
-            path: 'profile',
-            element: <Profile />
+            path: "profile",
+            element: <Profile />,
           },
           {
-            path: 'admin',
-            element: <Admin />
-          }
+            path: "admin",
+            element: <Admin />,
+          },
+          {
+            path: "edit-job/:id",
+            element: <EditJob />,
+          },
         ],
       },
-    ]
+    ],
   },
-])
+]);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
