ca1beae8855e9c4712d09291dd35d33d8850f942
vivek koyani <vivek.koyani@gmail.com>
2023-11-11 10:21:16 +0530

    profile page

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { FormRow } from "../components";
+import { useOutletContext, useNavigation, Form } from "react-router-dom";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
 
 function Profile() {
+  const { user } = useOutletContext();
+  const { name, lastName, email, location } = user;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
-    <h1>Profile</h1>
-  )
+    <Wrapper>
+      <Form method="post" className="form" encType="multipart/form-data">
+        <h4 className="form-title">Profile</h4>
+        <div className="form-center">
+          <div className="form-row">
+            <label htmlFor="avatar" className="form-label">
+              Select an image file (max 0.5 MB)
+            </label>
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              className="form-input"
+              accept="image/*"
+            />
+          </div>
+          <FormRow type="text" name="name" defaultValue={name} />
+          <FormRow
+            type="text"
+            name="lastName"
+            labelText="last name"
+            defaultValue={lastName}
+          />
+          <FormRow type="email" name="email" defaultValue={email} />
+          <FormRow type="text" name="location" defaultValue={location} />
+          <button
+            className="btn btn-block from-btn"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "submitting..." : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 }
 
-export default Profile
+export default Profile;
