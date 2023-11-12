e14daff47218a40b3b8f73b4a66d01481fa70679
vivek koyani <vivek.koyani@gmail.com>
2023-11-12 08:13:13 +0530

    miss char

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Form, redirect, ,Link } from 'react-router-dom'
+import {Form, redirect, Link } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo, SubmitBtn } from '../components';
 import customFetch from '../utils/customFetch';
