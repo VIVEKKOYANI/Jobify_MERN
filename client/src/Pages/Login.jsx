e7edffdd57db6fece84d3e2c74b89d251067fb9b
vivek koyani <vivek.koyani@gmail.com>
2023-11-12 08:27:48 +0530

    add test user

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo, SubmitBtn } from "../components";
-import { Link, Form, redirect} from "react-router-dom";
+import { Link, Form, redirect, useNavigate } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  const errors = {msg: ''}
-  if(data.password.length < 3){
-    errors.msg = 'password too short';
-    return errors
-  }
 
   try {
     await customFetch.post("/auth/login", data);
     toast.success("Login successful");
     return redirect("/dashboard");
   } catch (error) {
-    // toast.error(error?.response?.data?.msg);
-    errors.msg = error?.response?.data?.msg;
-    return errors;
+    toast.error(error?.response?.data?.msg);
+    return error;
   }
 };
 
 function Login() {
+  const navigate = useNavigate();
+
+  const loginDemoUser = async () => {
+    const data = {
+      email: "test@test.com",
+      password: "Vivek@123",
+    };
+    try {
+      await customFetch.post("/auth/login", data);
+      toast.success("Take a test drive");
+      navigate("/dashboard");
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+    }
+  };
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -38,7 +47,7 @@ function Login() {
         />
         <FormRow type="password" name="password" defaultValue="vivek123" />
         <SubmitBtn />
-        <button type="button" className="btn btn-block">
+        <button type="button" className="btn btn-block" onClick={loginDemoUser}>
           explore the app
         </button>
         <p>
